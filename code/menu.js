ff12e05c19c6ed3288a257380d78264ea9dec816
marker2026 <53335706+marker2026@users.noreply.github.com>
2020-05-09 19:20:29 +0200

    Add files via upload

--- code/menu.js
+++ code/menu.js
@@ -1,26 +1,26 @@
 let liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/index.html">На главную</a>';
-ul.append(liLast)
+navbar.append(liLast)
 
 liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/onlinemap/index.html">Онлайн карта</a>';
-ul.append(liLast)
+navbar.append(liLast)
 
 liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/donate/index.html">Донат на сервере</a>';
-ul.append(liLast)
+navbar.append(liLast)
 
 
 
 
 liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/sity/index.html">Города сервера</a>';
-ul.append(liLast)
+navbar.append(liLast)
 
 liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/apps/index.html">Приложения marker2026</a>';
-ul.append(liLast)
+navbar.append(liLast)
 
 liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/help_with_server.html">Помогали</a>';
-ul.append(liLast)
+navbar.append(liLast)
