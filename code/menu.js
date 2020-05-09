9cbe9515f0e53518208ad7f5db984d26bdcb9af5
marker2026 <53335706+marker2026@users.noreply.github.com>
2020-05-09 19:15:16 +0200

    Add files via upload

--- code/menu.js
+++ code/menu.js
@@ -2,25 +2,25 @@ let liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/index.html">На главную</a>';
 ul.append(liLast)
 
-let liLast = document.createElement('li');
+liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/onlinemap/index.html">Онлайн карта</a>';
 ul.append(liLast)
 
-let liLast = document.createElement('li');
+liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/donate/index.html">Донат на сервере</a>';
 ul.append(liLast)
 
 
 
 
-let liLast = document.createElement('li');
+liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/sity/index.html">Города сервера</a>';
 ul.append(liLast)
 
-let liLast = document.createElement('li');
+liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/apps/index.html">Приложения marker2026</a>';
 ul.append(liLast)
 
-let liLast = document.createElement('li');
+liLast = document.createElement('li');
 liLast.innerHTML = '<a href="http://marupik.ru/help_with_server.html">Помогали</a>';
 ul.append(liLast)
