9eafdcd220533fa3a06641fe9fd7897133b781e9
marker2026 <53335706+marker2026@users.noreply.github.com>
2020-05-09 16:41:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+document.write(`
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="icon" type="image/png" href="http://www.marupik.ru/favicon.png" sizes="50x50">
+		<meta http-equiv="Content-Type" content="charset=utf-8">
+		<title>Marupik</title>
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+	<link href="http://marupik.ru/css/style.css" rel="stylesheet">
+	</head>
+    <body>
+		<div class="wrapper">
+			<header class="header">
+                <div class="m">
+                    <link rel="import" href="http://marupik.ru/code/menu.html">
+				</div>
+                </header><!-- .header-->
+            
+                <div class="middle">
+            
+                    <div class="container">
+                        <br><p class="header_marupik">Marupik</p><br><br>
+                        <p>Marupik это ванильный, бесплатный сервер с элементами RolePlay(РП) на последних версиях.<br>
+                        Когда вы зайдёте на сервер Вы сможете поселится у кого-то в городе, или создать свой. Столица находится в грибном биоме(4578 -1979)</p>
+                    </div>
+            
+                    <aside class="left-sidebar">
+                        <h6>Новости:</h6><br><p>Теперь сервер находится на mc.marupik.ru<br>Требуется помощь с спаваном.</p>
+                    </aside><!-- .left-sidebar -->
+            
+                </div><!-- .middle-->
+            
+                <footer class="footer">
+                    <strong>Autor:</strong><p>marker2026 ©</p>
+                </footer><!-- .footer -->
+            
+            </div><!-- .wrapper -->
+        </body>
+</html>`);
