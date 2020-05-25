e3052abc6f38e8513620aae25e439a08e4cecd23
marker2026 <53335706+marker2026@users.noreply.github.com>
2020-05-25 07:12:42 +0200

    Add files via upload

--- code/news.js
+++ code/news.js
@@ -1,3 +1,3 @@
 let p_ = document.createElement('p');
-p_.innerHTML = "Теперь сервер находится на mc.marupik.ru<br>Требуется помощь с спаваном."
+p_.innerHTML = "Был вайп<br>Требуется помощь с спаваном."
 news.append(p_);
